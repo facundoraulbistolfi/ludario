1e5c0a9cbe3cfb95d66f2a4c7c38b446336c434e
Facundo Bistolfi <64446909+facundoraulbistolfi@users.noreply.github.com>
2026-03-30 18:15:50 -0300

    Restore Chinchon Arena styling utilities

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import './styles/chinchon-arena-utilities.css'
 import './styles/site.css'
 
 createRoot(document.getElementById('root')!).render(
